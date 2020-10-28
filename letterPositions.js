f946372af55fda2fae6363b752d3d8988bbbb7c2
cambrown101 <cameron-brown@live.com>
2020-10-28 14:47:27 -0700

    Function to give index's of a string per char

---
+++ letterPositions.js
@@ -0,0 +1,46 @@
+const assertArraysEqual = (arr1, arr2) => {
+  let results = arr1.length === arr2.length;
+  if (results === false) {
+    console.log(`🛑🛑🛑Assertion Failed: [${arr1}] !== [${arr2}]`);
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      results === true ? (results = arr1[i] === arr2[i]) : (results = false);
+    }
+  }
+  results
+    ? console.log(`✅✅✅Assertion Passed: [${arr1}] === [${arr2}]`)
+    : console.log(`🛑🛑🛑Assertion Failed: [${arr1}] !== [${arr2}]`);
+  return results;
+};
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const letterPositions = (sentence) => {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== ' ') {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+assertArraysEqual(letterPositions('hello').e, [1]);
+assertArraysEqual(letterPositions('hello').l, [2, 3]);
+assertArraysEqual(letterPositions('hello').o, [4]);
+assertArraysEqual(letterPositions('hello world').o, [4, 7]);
