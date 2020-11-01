94ab6b189795d64bbef9d9208bf41e8d60e853fa
cambrown101 <cameron-brown@live.com>
2020-11-01 14:49:28 -0800

    add recursion to compare arrays in arrays

--- eqArrays.js
+++ eqArrays.js
@@ -6,35 +6,40 @@ const assertEqual = (actual, expected) => {
   }
 };
 
-// const eqArrays = (arr1, arr2) => {
-//   let results = arr1.length === arr2.length;
-//   if (results === false) {
-//     return false;
-//   } else {
-//     for (let i = 0; i < arr1.length; i++) {
-//       results === true ? (results = arr1[i] === arr2[i]) : (results = false);
-//     }
-//   }
-//   return results;
-// };
 
 const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) {
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+    if (Array.isArray(arr1[i])) {
+      return eqArrays(arr1[i], arr2[i])
+    } else if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+assertEqual(eqArrays(["1", 2, [3, 2, [2, 1]]], [1, 2, [3, 2, [2, 1]]]), false); // => true
+// //loop through the keys and values of one of the objects
+// for (const [key, value] of Object.entries(obj1)) {
+//   //check to see if any of those values are objects
+//   if (typeof value === "object") {
+//     //if a value is an object, recall the eqObjects function again and start again
+//     if (!eqObjects(obj1[key], obj2[key])) return false;
+//   } else {
+//     // if not an object check to see if the keys from the two objects match
+//     if (obj1[key] !== obj2[key]) return false;
+//   }
 
-assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true); // => true
-assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false); // => false
+// }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [2, 3, 3]), false); // => fail
+
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+// assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true); // => true
+// assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false); // => false
+
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// assertEqual(eqArrays([1, 2, 3], [2, 3, 3]), false); // => fail
