fc077728d0d930d300843355f51e1fc5a71f84db
cambrown101 <cameron-brown@live.com>
2020-11-02 14:40:30 -0800

    removed assertEquals

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,5 @@
 const head = require('../head')
 const assert = require('chai').assert;
-const assertEqual = require('../assertEqual')
 
 describe("#head", () => {
     it("returns 1 for [1, 2, 3]", () => {
