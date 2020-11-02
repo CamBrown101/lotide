1af529415e34738b1e31e97b7ce5265b900a5e49
cambrown101 <cameron-brown@live.com>
2020-11-02 12:57:19 -0800

    exported the function and cleaned it up

--- assertEqual.js
+++ assertEqual.js
@@ -6,9 +6,4 @@ const assertEqual = (actual, expected) => {
   }
 };
 
-assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual(1, 1);
-assertEqual(false, 65);
-assertEqual('Lets Go', 'Lets Go');
-assertEqual('Hello World', 'Hello World');
-assertEqual(12, 32);
+module.exports = assertEqual;
