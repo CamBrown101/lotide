61f35cdaa502ebadc02581f66e8a0db527a80c33
cambrown101 <cameron-brown@live.com>
2020-11-02 12:57:37 -0800

    moved tests for assertEqual to new file

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual')
+
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1, 1);
+assertEqual(false, 65);
+assertEqual('Lets Go', 'Lets Go');
+assertEqual('Hello World', 'Hello World');
+assertEqual(12, 32);
