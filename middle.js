9af925c72279ab6199993417ecf09c03b3ec556b
cambrown101 <cameron-brown@live.com>
2020-11-02 13:51:29 -0800

    cleaned up middle, exported, created seperate test

--- middle.js
+++ middle.js
@@ -1,31 +1,4 @@
-const assertArraysEqual = (arr1, arr2) => {
-  console.log(arr1, arr2);
-  let results = arr1.length === arr2.length;
-  if (results === false) {
-    console.log(`🛑🛑🛑Assertion Failed: [${arr1}] !== [${arr2}]`);
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      results === true ? (results = arr1[i] === arr2[i]) : (results = false);
-    }
-  }
-  results
-    ? console.log(`✅✅✅Assertion Passed: [${arr1}] === [${arr2}]`)
-    : console.log(`🛑🛑🛑Assertion Failed: [${arr1}] !== [${arr2}]`);
-  return results;
-};
-
-// const eqArrays = (arr1, arr2) => {
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   }
-//   for (let i = 0; i < arr1.length; i++) {
-//     if (arr1[i] !== arr2[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
+const assertArraysEqual = require('./assertArraysEqual')
 
 const middle = (arr) => {
   let returnArr = [];
@@ -35,15 +8,9 @@ const middle = (arr) => {
     returnArr.push(arr[arr.length / 2 - 1]);
     returnArr.push(arr[arr.length / 2]);
   } else if (arr.length % 2 !== 0) {
-    console.log(arr.length / 2);
     returnArr.push(arr[Math.floor(arr.length / 2)]);
   }
   return returnArr;
 };
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+module.exports = middle;
